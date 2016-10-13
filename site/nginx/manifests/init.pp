40da70fad0efc027322c40454e429489ece2566d
karanpuppet <karan.arora@aon.com>
2016-10-13 23:36:45 +0530

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -43,4 +43,12 @@ service { 'nginx':
 ensure => running,
 enable => true,
 }
+yumrepo { 'updates':
+  ensure     => 'present',
+  descr      => 'CentOS-$releasever - Updates',
+  enabled    => '1',
+  gpgcheck   => '1',
+  gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
+  mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates&infra=$infra',
+}
 }
