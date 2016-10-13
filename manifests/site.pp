9377e01a53615230e2863b144add9722e619b800
karanpuppet <karan.arora@aon.com>
2016-10-13 21:32:28 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,7 +38,7 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-node default {
+#node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
@@ -55,5 +55,5 @@ node default {
   #}
   #include role::classroom
   #include users
-include memcached
-}
+#include memcached
+#}
